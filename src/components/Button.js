932399f2aa0a77242ca9289c8481c6a6d26ca489
elianiva <dicha.arkana03@gmail.com>
2020-06-15 12:49:49 +0700

    added svg icon for button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,21 @@
 import React from "react"
 import PropType from "prop-types"
+import Trash from "../assets/trash.svg"
+import Edit from "../assets/edit.svg"
 
-export default function Button({ type }) {
+export default function Button({ type, onClick }) {
+  if (onClick) onClick()
   switch (type) {
-    case "add":
+    case "edit":
       return (
-        <div className="w-full p-2 mx-1 rounded-md my-4 bg-green-300 hover:bg-green-400 text-blue-800 cursor-pointer font-bold">
-          +
+        <div className="w-full p-2 mx-1 rounded-md my-4 bg-green-300 hover:bg-green-400 text-blue-800 cursor-pointer font-bold flex items-center justify-center">
+          <Edit />
         </div>
       )
     case "remove":
       return (
-        <div className="w-full p-2 mx-1 rounded-md my-4 bg-red-300 hover:bg-red-400 text-blue-800 cursor-pointer font-bold">
-          -
+        <div className="w-full p-2 mx-1 rounded-md my-4 bg-red-300 hover:bg-red-400 text-blue-800 cursor-pointer font-bold flex items-center justify-center">
+          <Trash />
         </div>
       )
     case "table":
@@ -27,5 +30,6 @@ export default function Button({ type }) {
 }
 
 Button.propTypes = {
-  type: PropType.string.isRequired
+  type: PropType.string.isRequired,
+  onClick: PropType.func.isRequired
 }
