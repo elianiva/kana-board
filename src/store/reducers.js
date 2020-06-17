33169006776c267ecd5573af61a4f4f005545d29
elianiva <dicha.arkana03@gmail.com>
2020-06-17 21:47:01 +0700

    added UUID package

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -8,9 +8,9 @@ import {
 } from "./types"
 import { format } from "date-fns"
 import { combineReducers } from "redux"
+import { v4 as uuidv4 } from "uuid"
 
 export const initialState = {
-  addTable: false,
   addItem: false,
   items: [
     {
@@ -23,7 +23,7 @@ export const initialState = {
           meaning: "cute",
           example: ["かわいいね", "Cute isn't it?"],
           context: "Used to say if something is cute or not",
-          ownExample: ["かわいいです", "It's cute"]
+          myExample: ["かわいいです", "It's cute"]
         },
         {
           _id: "ec5ca2fa-9003-455e-9bc1-abfaeee7d60b",
@@ -31,7 +31,7 @@ export const initialState = {
           meaning: "cute",
           example: ["かわいいね", "Cute isn't it?"],
           context: "Used to say if something is cute or not",
-          ownExample: ["かわいいです", "It's cute"]
+          myExample: ["かわいいです", "It's cute"]
         },
         {
           _id: "839160f6-5936-4167-8f4b-3329156d93ad",
@@ -39,40 +39,40 @@ export const initialState = {
           meaning: "cute",
           example: ["かわいいね", "Cute isn't it?"],
           context: "Used to say if something is cute or not",
-          ownExample: ["かわいいです", "It's cute"]
+          myExample: ["かわいいです", "It's cute"]
         }
       ]
     }
   ]
 }
 
-const colReducer = (cols = [], action) => {
+const colReducer = (cols, action) => {
   switch (action.type) {
     case ADD_ITEM:
       return [
         ...cols,
         {
-          _id: "uuid",
+          _id: uuidv4(),
           kana: action.data.kana,
           meaning: action.data.meaning,
           example: action.data.example,
           context: action.data.context,
-          ownExample: action.data.ownExample
+          myExample: action.data.myExample
         }
       ]
     case REMOVE_ITEM:
-      return cols.filter(id => id !== action.id)
+      return cols.filter(col => col._id !== action.payload.id)
     case EDIT_ITEM:
       let filtered = cols.filter(id => id !== action.id)
       return [
         ...filtered,
         {
-          _id: "uuid",
+          _id: uuidv4(),
           kana: action.data.kana,
           meaning: action.data.meaning,
           example: action.data.example,
           context: action.data.context,
-          ownExample: action.data.ownExample
+          myExample: action.data.myExample
         }
       ]
     default:
@@ -84,36 +84,34 @@ export const tableReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TABLE:
       return {
+        ...state,
         items: [
           ...state.items,
           {
-            _id: "dfb5871c-e260-4e76-98f1-92db862c2741",
+            _id: uuidv4(),
             date: format(new Date(), "iiii, dd MMMM yyyy"),
             cols: []
           }
         ]
       }
-    case ADD_ITEM || EDIT_ITEM || REMOVE_ITEM:
+    case REMOVE_ITEM:
       return {
+        ...state,
         items: state.items.map(item => {
-          item._id === action.payload.id
-            ? {
-                _id: item._id,
-                date: item.date,
-                cols: colReducer(item.col, action.payload.text)
-              }
-            : item
+          // if (item._id == action.payload.id) {
+          return {
+            _id: item._id,
+            date: item.date,
+            cols: colReducer(item.cols, action)
+          }
+          // }
+          // return item
         })
       }
     case TOGGLE_ADD_ITEM:
       return {
         ...state,
         addItem: !state.addItem
-      }
-    case TOGGLE_ADD_TABLE:
-      return {
-        ...state,
-        addTable: !state.addTable
       }
     default:
       return state
