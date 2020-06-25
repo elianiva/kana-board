62ec36dcdf8d863dae11b3302fa0857c93bc3bcf
elianiva <dicha.arkana03@gmail.com>
2020-06-25 20:22:19 +0700

    removed unused div

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,19 +3,17 @@ import PropType from "prop-types"
 
 export default function Form({ label, name, placeholder, value, onChange }) {
   return (
-    <div className="m-2 text-left">
-      <label htmlFor={name}>
-        {label}
-        <input
-          type="text"
-          name={name}
-          className="block p-1 border-2 w-full mt-2 rounded rounded-md"
-          placeholder={placeholder}
-          value={value}
-          onChange={onChange}
-        />
-      </label>
-    </div>
+    <label className="m-2 text-left">
+      {label}
+      <input
+        type="text"
+        name={name}
+        className="block p-1 border-2 w-full mt-2 rounded rounded-md"
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </label>
   )
 }
 
